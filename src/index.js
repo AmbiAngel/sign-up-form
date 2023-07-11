e8ae48840c9f557dd7392df869b9f9822c37c9f6
Angel <angelambisilva@gmail.com>
2023-07-11 17:37:36 -0400

    Add html and basic css styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css'
+import leafBG from './imgs/leaf_bg.png'
+import angelIMG from "./imgs/angel.jpg"
+// import Pope from './ai_pope_drip_god.png'
+
+let img = new Image()
+img.src = leafBG
+// document.querySelector('body').appendChild(img)
