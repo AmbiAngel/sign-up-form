5748f4a050271c6d53b3d0badab254aef2f3192f
Angel <angelambisilva@gmail.com>
2023-07-11 19:07:37 -0400

    Remove more excess comment and update readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sign-up-form
+This is an exercise to:
+    replicate a form design given to us
+    Perform some input validation through JS
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import './style.css'
 import leafBG from './imgs/leaf_bg.png'
 import angelIMG from "./imgs/angel.jpg"
-// import Pope from './ai_pope_drip_god.png'
 
 let img = new Image()
 img.src = leafBG
-// document.querySelector('body').appendChild(img)
 
-// let button = document.querySelector("button")
-
-// button.addEventListener('click', stopSubmitForm)
-// function stopSubmitForm(e){
-//     e.preventDefault()
-// }
 
 let pwd = document.querySelector('#password')
 let confirmPwd = document.querySelector('#confirm-password')
