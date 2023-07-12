83e54cd46947525015f4aee546ce239a77f574a0
Angel <angelambisilva@gmail.com>
2023-07-12 14:59:16 -0400

    Remove debug logs from js

--- src/index.js
+++ src/index.js
@@ -15,22 +15,15 @@ confirmPwd.addEventListener('keyup', validateConfirmPwd)
 function validateConfirmPwd(e){
     console.log(`pwd: ${pwd.value}\nconf:${e.target.value}`);
     if (e.target.value !== pwd.value){
-        console.log('pwds do not match');
         confirmPwdMsg.textContent = 'passwords do not match'
         confirmPwdMsg.classList.remove('color-green')
         this.setCustomValidity('Passwords do not match')
     }
     else{
-        console.log('pwds match');
         confirmPwdMsg.textContent = 'passwords match'
         confirmPwdMsg.classList.add('color-green')
         this.setCustomValidity('')
 
 
     }
-    
-    console.log(this.reportValidity());
-    console.log(e.target.reportValidity());
-    console.log(this.checkValidity());
-    console.log(e.target.checkValidity());
 }
